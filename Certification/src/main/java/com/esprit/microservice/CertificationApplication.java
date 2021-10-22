c4afe958ecadc44ee5f5857e3ee2ba34925d2e4d
Hazem <hazem.cherif@esprit.tn>
2021-10-22 20:05:31 +0100

    docker

--- Certification/src/main/java/com/esprit/microservice/CertificationApplication.java
+++ Certification/src/main/java/com/esprit/microservice/CertificationApplication.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-@EnableEurekaClient
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CertificationApplication {
 	
 	public static void main(String[] args) {
--- UserMicroService/src/main/java/com/esprit/microservice/UserMicroServiceApplication.java
+++ UserMicroService/src/main/java/com/esprit/microservice/UserMicroServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserMicroServiceApplication {
 	
 	public static void main(String[] args) {
